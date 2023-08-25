3d9abe6148611fe437ca29185c04499af6ef59bf
Bernardo Ayala <ba@bernardoayala.com>
2023-08-25 12:27:10 -0400

    remove pandas and work purely with openpyxl

--- excel_data_transfer.py
+++ excel_data_transfer.py
@@ -1,26 +1,44 @@
-import pandas as pd
+from datetime import datetime
 import openpyxl
 import sys
 
 def main(argv):
     filename = argv[1]
-    start = int(argv[2]) - 2
-    end = int(argv[3]) - 1
+    start = int(argv[2])
+    end = int(argv[3]) + 1
 
-    xlsx = pd.ExcelFile(filename, engine='openpyxl')
-    df = xlsx.parse(xlsx.sheet_names[0])
-
-    # Copy and format the columns of interest
-    formatted_df = df.iloc[start:end, [1, 6, 13, 17]].copy()
-    formatted_df.iloc[:, 0] = pd.to_datetime(formatted_df.iloc[:, 0], format='%d/%m/%Y').dt.date
-    formatted_df.iloc[:, 2] = pd.to_numeric(formatted_df.iloc[:, 2].astype(str).str.replace(',', ''), errors='coerce')
-    formatted_df.iloc[:, 3] = pd.to_numeric(formatted_df.iloc[:, 3].astype(str).str.replace(',', ''), errors='coerce')
-
-    # Here we reverse the order of the rows
-    formatted_df = formatted_df.iloc[::-1]
-
-    with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
-        formatted_df.to_excel(writer, sheet_name='Formatted data', index=False)
+    wb = openpyxl.load_workbook(filename)
+    # Store data from the start to the end rows inside the B, G, N and R excel columns
+    columns_index = [2, 7, 14, 18]
+    data = []
+    # Create new sheet called "Formatted data"
+    wb.create_sheet("Formatted data")
+    for i in range(start, end):
+        row = []
+        for j in columns_index:
+            ## if column is 2, convert date string which this format DD/MM/YYYY to a date object
+            ## and change its format to MM/DD/YYYY without leading zeros
+            if j == 2:
+                date_list = wb.active.cell(row=i, column=j).value.split("/")
+                year = int(date_list[2])
+                month = int(date_list[1])
+                day = int(date_list[0])
+                date = datetime(year, month, day).strftime("%m-%d-%Y")
+                row.append(date)
+            ## if colum is 14 or 18, convert text with ',' to float
+            elif j == 14 or j == 18:
+                row.append(float(wb.active.cell(row=i, column=j).value.replace(",", "")))
+            else:
+                row.append(wb.active.cell(row=i, column=j).value)
+        data.append(row)
+    
+    # Invert data list so oldest entries are at the top
+    data.reverse()
+    # Append the row to the new sheet in inverse order
+    for row in data:
+        wb["Formatted data"].append(row)
+    # Save the new sheet
+    wb.save(filename)
 
 if __name__ == "__main__":
     main(sys.argv)
